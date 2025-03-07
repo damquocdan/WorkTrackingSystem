e65133d19a32e73bdb1106c4c8738df789423667
minhhai25 <ntmhai25@gmail.com>
2025-03-07 09:58:04 +0700

    3/7 updateemployee

--- WorkTrackingSystem/Areas/HRManager/Controllers/EmployeesController.cs
+++ WorkTrackingSystem/Areas/HRManager/Controllers/EmployeesController.cs
@@ -25,29 +25,47 @@ namespace WorkTrackingSystem.Areas.HRManager.Controllers
         public async Task<IActionResult> Index( string search,int? DepartmentId,int page = 1)
         {
             var limit = 5;
-            var employees = _context.Employees.Include(e => e.Department).Include(e => e.Position).ToPagedList(page,limit);
-            ViewBag.Department = new SelectList(_context.Departments, "Id", "Name");
-            if (DepartmentId > 0)
-            {
-                employees = employees.Where(e => e.DepartmentId == DepartmentId).ToPagedList(page, limit);
-            }
-            if (!string.IsNullOrEmpty(search))
-            {
-                var searchLower = search.ToLower();
-                employees = _context.Employees
-                   .Where(e =>
-                   (e.FirstName + " " + e.LastName).ToLower().Contains(searchLower))
-                   .Include(e => e.Department)
-                   .ToPagedList(page, limit);
-                return View(employees);
-            }
-            return View(employees);
-            //    var workTrackingSystemContext = _context.Employees.Include(e => e.Department).Include(e => e.Position);
-            //return View(await workTrackingSystemContext.ToListAsync());
-        }
+			var query = _context.Employees.Include(e => e.Department).Include(e => e.Position).AsQueryable();
 
-        // GET: HRManager/Employees/Details/5
-        public async Task<IActionResult> Details(long? id)
+			if (DepartmentId > 0)
+			{
+				query = query.Where(e => e.DepartmentId == DepartmentId);
+			}
+
+			if (!string.IsNullOrEmpty(search))
+			{
+				var searchLower = search.ToLower();
+				query = query.Where(e => (e.FirstName + " " + e.LastName).ToLower().Contains(searchLower));
+			}
+
+			var employees =  query.ToPagedList(page, limit);
+			ViewBag.Department = new SelectList(_context.Departments, "Id", "Name");
+
+			return View(employees);
+
+			//var employees = _context.Employees.Include(e => e.Department).Include(e => e.Position).ToPagedList(page,limit);
+			//ViewBag.Department = new SelectList(_context.Departments, "Id", "Name");
+			//if (DepartmentId > 0)
+			//{
+			//    employees = employees.Where(e => e.DepartmentId == DepartmentId).ToPagedList(page, limit);
+			//}
+			//if (!string.IsNullOrEmpty(search))
+			//{
+			//    var searchLower = search.ToLower();
+			//    employees = _context.Employees
+			//       .Where(e =>
+			//       (e.FirstName + " " + e.LastName).ToLower().Contains(searchLower))
+			//       .Include(e => e.Department)
+			//       .ToPagedList(page, limit);
+			//    return View(employees);
+			//}
+			//return View(employees);
+			//    var workTrackingSystemContext = _context.Employees.Include(e => e.Department).Include(e => e.Position);
+			//return View(await workTrackingSystemContext.ToListAsync());
+		}
+
+		// GET: HRManager/Employees/Details/5
+		public async Task<IActionResult> Details(long? id)
         {
             if (id == null)
             {
