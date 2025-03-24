8b41987acc5c5587ce6b498f2f36f9c59becec2a
minhhai25 <ntmhai25@gmail.com>
2025-03-24 10:21:08 +0700

    adm avatar

--- WorkTrackingSystem/Areas/AdminSystem/Controllers/EmployeesController.cs
+++ WorkTrackingSystem/Areas/AdminSystem/Controllers/EmployeesController.cs
@@ -155,7 +155,7 @@ namespace WorkTrackingSystem.Areas.AdminSystem.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(long id, [Bind("Id,DepartmentId,PositionId,Code,FirstName,LastName,Gender,Birthday,Phone,Email,HireDate,Address,IsDelete,IsActive,CreateDate,UpdateDate,CreateBy,UpdateBy")] Employee employee)
+        public async Task<IActionResult> Edit(long id, [Bind("Id,DepartmentId,PositionId,Code,FirstName,LastName,Gender,Birthday,Phone,Email,HireDate,Address,IsDelete,IsActive,CreateDate,UpdateDate,CreateBy,UpdateBy")] Employee employee, string? img)
         {
             if (id != employee.Id)
             {
@@ -166,9 +166,68 @@ namespace WorkTrackingSystem.Areas.AdminSystem.Controllers
             {
                 try
                 {
-                    employee.UpdateDate = DateTime.Now;
-                    employee.UpdateBy = HttpContext.Session.GetString("AdminLogin");
-                    _context.Update(employee);
+                    var existingEmployee = await _context.Employees
+                   .Include(e => e.Department)
+                   .Include(e => e.Position)
+                   .FirstOrDefaultAsync(e => e.Id == id);
+
+                    if (existingEmployee == null)
+                    {
+                        return NotFound("Không tìm thấy thông tin nhân viên.");
+                    }
+
+                    // Cập nhật thông tin nhân viên
+                    existingEmployee.FirstName = employee.FirstName;
+                    existingEmployee.LastName = employee.LastName;
+                    existingEmployee.Gender = employee.Gender;
+                    existingEmployee.Birthday = employee.Birthday;
+                    existingEmployee.Phone = employee.Phone;
+                    existingEmployee.Email = employee.Email;
+                    existingEmployee.HireDate = employee.HireDate;
+                    existingEmployee.Address = employee.Address;
+                    existingEmployee.DepartmentId = employee.DepartmentId;
+                    existingEmployee.PositionId = employee.PositionId;
+                    //existingEmployee.IsActive = employee.IsActive;
+                    existingEmployee.UpdateBy = HttpContext.Session.GetString("AdminLogin");
+                    existingEmployee.UpdateDate = DateTime.Now;
+                    
+
+
+                    // Xử lý avatar
+                    var files = HttpContext.Request.Form.Files;
+                    if (files.Count > 0 && files[0].Length > 0)
+                    {
+                        var file = files[0];
+                        var fileName = $"{Guid.NewGuid()}_{Path.GetFileName(file.FileName)}"; // Đặt tên duy nhất
+                        var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/employees", fileName);
+
+                        // Xóa ảnh cũ nếu có
+                        if (!string.IsNullOrEmpty(existingEmployee.Avatar))
+                        {
+                            var oldPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", existingEmployee.Avatar.TrimStart('/'));
+                            if (System.IO.File.Exists(oldPath))
+                            {
+                                System.IO.File.Delete(oldPath);
+                            }
+                        }
+
+                        // Lưu ảnh mới
+                        using (var stream = new FileStream(path, FileMode.Create))
+                        {
+                            await file.CopyToAsync(stream);
+                        }
+
+                        existingEmployee.Avatar = "/images/employees/" + fileName;
+                    }
+                    else
+                    {
+                        existingEmployee.Avatar = img;
+                    }
+                    //employee.UpdateDate = DateTime.Now;
+                    //employee.UpdateBy = HttpContext.Session.GetString("AdminLogin");
+
+                    //_context.Update(employee);
+                    _context.Update(existingEmployee);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
